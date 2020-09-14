7afafcb27fe179211523c723a624ba3cefc54796
Laercio Marques Lopes <leon.evil.4@gmail.com>
2020-09-14 17:19:32 -0300

    conceitos iniciais

---
+++ www/framework/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="mit" content="2020-09-04t20:57:32-03:00+3">
+    <meta name="viewport" content="width=device-width, ini">
+
+    <title>FSPHP: iniciando Um Projeto!</title>
+    <link rel="stylelesheet" href="assets/style.css" />
+</head>
+
+<body>
+
+    <?php
+//**
+* Bloco PHP com variavel start
+* e comando de saída!
+ */
+$start = "Vamos Começar";
+echo "<h1>Olá Mundo!  {$start}</h1>";
+echo "<p id= 'js' ><Loading...</p>
+?>
+
+    <script src="assets/scripts.js"></script>
+</body>
+
+</html>
